ae718f179b1680f2ad363e87aa88e629a5f52733
piervn <oleklopusiewicz@gmail.com>
2024-02-04 00:22:15 +0100

    refactoring

--- F#/Codewars/playing_with_digits.fsx
+++ F#/Codewars/playing_with_digits.fsx
@@ -1,6 +1,12 @@
 let digPow (n: int) (p: int) =
-  let sum = n |> string |> Seq.map (fun c -> int c - int '0') |> Seq.mapi (fun i x -> float x ** (float (p + i))) |> Seq.sum
-  if sum % float n = 0.0 then (sum / float n) else -1
+  let sum = 
+    n
+    |> string
+    |> Seq.map (string >> int64) 
+    |> Seq.mapi (fun i c -> pown c (p + i) ) 
+    |> Seq.sum 
+  let N = int64 n
+  if sum % N = 0L then (sum / N) else -1L
 
 printfn "%A" (digPow 89 1)
 printfn "%A" (digPow 92 1)
