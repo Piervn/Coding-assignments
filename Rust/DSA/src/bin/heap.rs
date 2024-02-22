8ba7f95a96cbc341fc8be190e4c9c5744e741806
piervn <oleklopusiewicz@gmail.com>
2024-02-23 00:04:18 +0100

    Rust: heapsort

--- Rust/DSA/src/bin/heap.rs
+++ Rust/DSA/src/bin/heap.rs
@@ -1,11 +1,11 @@
 use std::fmt;
 
-enum HeapType {
+pub enum HeapType {
     MIN,
     MAX,
 }
 
-struct Heap<T> {
+pub struct Heap<T> {
     heap_type: HeapType,
     data: Vec<T>,
 }
@@ -49,7 +49,7 @@ impl<T: Ord> Heap<T> {
         self.data.first()
     }
 
-    pub fn heapify_up(&mut self, i: usize) {
+    fn heapify_up(&mut self, i: usize) {
         match self.parent(i) {
             Some(parent) => {
                 if !self.compare(parent, i) {
@@ -61,7 +61,7 @@ impl<T: Ord> Heap<T> {
         }
     }
 
-    pub fn heapify_down(&mut self, i: usize) {
+    fn heapify_down(&mut self, i: usize) {
         match self.children(i) {
             (Some(left), Some(right)) => {
                 let (left_valid, right_valid) = (self.compare(i, left), self.compare(i, right));
@@ -114,6 +114,10 @@ impl<T: Ord> Heap<T> {
             HeapType::MAX => self.data[left] > self.data[right],
         }
     }
+
+    fn swap(&mut self, left: usize, right: usize) {
+        self.data.swap(left, right);
+    }
 }
 
 impl<T: fmt::Debug> fmt::Display for Heap<T> {
@@ -122,20 +126,113 @@ impl<T: fmt::Debug> fmt::Display for Heap<T> {
     }
 }
 
+fn heapsort<T: Ord + Clone>(data: &mut [T]) {
+    let mut heap = Heap::from_vec(HeapType::MAX, data.to_vec());
+    for i in (0..data.len()).rev() {
+        data[i] = heap.pop().unwrap();
+    }
+}
+
+// We save here one comparision for each heapify_down
+fn heapsort_with_hole<T: Ord + Clone + fmt::Debug>(data: &mut [T]) {
+    let mut heap = Heap::from_vec(HeapType::MAX, data.to_vec());
+    for i in (0..data.len()).rev() {
+        let mut current = 0;
+        loop {
+            match heap.children(current) {
+                (Some(left), Some(right)) => {
+                    // 1 comparision!
+                    if heap.compare(left, right) {
+                        heap.swap(current, left);
+                        current = left;
+                    } else {
+                        heap.swap(current, right);
+                        current = right;
+                    }
+                }
+                (Some(left), None) => {
+                    heap.swap(current, left);
+                    current = left;
+                }
+                _ => break,
+            }
+        }
+        if current != i {
+            heap.swap(current, i);
+            heap.heapify_up(current);
+        }
+        data[i] = heap.data[i].clone();
+        heap.data.truncate(i);
+    }
+}
+
 fn main() {
-    let mut heap = Heap::new(HeapType::MAX);
-    heap.push(5);
-    heap.push(3);
-    heap.push(7);
-    heap.push(1);
-    heap.push(4);
-    heap.push(2);
-    heap.push(6);
-    heap.push(8);
+    let heap = Heap::from_vec(HeapType::MAX, vec![5, 3, 7, 1, 4, 2, 6, 8]);
     println!("{}", heap);
-    println!("{:?}", heap.pop());
-    println!("{}", heap);
-    let data = vec![5, 3, 7, 1, 4, 2, 6, 8];
-    let heap_from_vec = Heap::from_vec(HeapType::MAX, data);
-    println!("{}", heap_from_vec);
+}
+
+#[cfg(test)]
+mod heap_tests {
+    #[test]
+    fn min_heap_test() {
+        let mut heap = super::Heap::new(super::HeapType::MIN);
+        heap.push(5);
+        heap.push(3);
+        heap.push(7);
+        heap.push(1);
+        heap.push(4);
+        heap.push(2);
+        heap.push(6);
+        heap.push(8);
+        while heap.data.len() > 0 {
+            let root = heap.peek().unwrap();
+            assert!(root <= heap.data.iter().max().unwrap());
+            heap.pop().unwrap();
+        }
+    }
+
+    #[test]
+    fn max_heap_test() {
+        let mut heap = super::Heap::new(super::HeapType::MAX);
+        heap.push(5);
+        heap.push(3);
+        heap.push(7);
+        heap.push(1);
+        heap.push(4);
+        heap.push(2);
+        heap.push(6);
+        heap.push(8);
+        while heap.data.len() > 0 {
+            let root = heap.peek().unwrap();
+            assert!(root >= heap.data.iter().min().unwrap());
+            heap.pop().unwrap();
+        }
+    }
+
+    #[test]
+    fn heap_from_vec_test() {
+        let data = vec![5, 3, 7, 1, 4, 2, 6, 8];
+        let mut heap_from_vec = super::Heap::from_vec(super::HeapType::MAX, data);
+
+        while heap_from_vec.data.len() > 0 {
+            let root = heap_from_vec.peek().unwrap();
+            assert!(root >= heap_from_vec.data.iter().min().unwrap());
+            heap_from_vec.pop().unwrap();
+        }
+    }
+
+    #[test]
+    fn heapsort_test() {
+        let mut data = vec![5, 3, 7, 1, 4, 2, 6, 8];
+        super::heapsort(&mut data);
+        assert_eq!(data, vec![1, 2, 3, 4, 5, 6, 7, 8]);
+    }
+
+    #[test]
+    fn heapsort_with_hole_test() {
+        let mut data = vec![4, 2, 6, 7, 1, 3, 5, 8];
+        super::heapsort_with_hole(&mut data);
+        //println!("{:?}", data);
+        assert_eq!(data, vec![1, 2, 3, 4, 5, 6, 7, 8]);
+    }
 }
