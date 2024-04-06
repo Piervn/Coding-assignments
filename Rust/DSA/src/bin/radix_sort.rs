3ecc8d08e120d19949da9e7357b323aea35e7cf0
piervn <oleklopusiewicz@gmail.com>
2024-04-06 23:53:35 +0200

    Rust: radix sort WIP

--- Rust/DSA/src/bin/radix_sort.rs
+++ Rust/DSA/src/bin/radix_sort.rs
@@ -42,8 +42,49 @@ fn radix_sort_same_length(strings: &mut Vec<&str>) {
     }
 }
 
+fn radix_sort(strings: &mut Vec<&str>) {
+    let mut not_empty_buckets: Vec<Vec<usize>> = vec![vec![]; 26];
+    let mut letter_positions: Vec<(usize, char)> =
+        strings
+            .iter()
+            .flat_map(|string| string.chars().enumerate())
+            .collect();
+
+    // sorting letter posotions vector
+    let mut letter_buckets: Vec<Vec<(usize, char)>> = vec![vec![]; 26]; 
+    for letter_pos in letter_positions.iter() {
+        let id = letter2num(letter_pos.1);
+        letter_buckets[id].push(*letter_pos);
+    }
+    let mut index = 0;
+    for bucket in letter_buckets.iter_mut() {
+        for letter_pos in bucket.iter() {
+            letter_positions[index] = *letter_pos;
+            index += 1;
+        }
+        bucket.clear();
+    }
+    let mut position_buckets: Vec<Vec<usize>> = vec![vec![]; 26];
+    for letter_pos in letter_positions.iter() {
+        let id = letter2num(letter_pos.1);
+        position_buckets[id].push(letter_pos.0);
+    }
+    let mut index = 0;
+    for bucket in position_buckets.iter_mut() {
+        for letter_pos in bucket.iter() {
+            not_empty_buckets[*letter_pos].push(index);
+            index += 1;
+        }
+        bucket.clear();
+    }
+
+}
+
 fn main() {
     let mut strings = vec!["xyzw", "zone", "bbba", "abcd", "bbbb", "gfre", "acdf", "qwer", "bgfd", "zsdw"];
     radix_sort_same_length(&mut strings);
     println!("{:?}", strings);
+
+    let mut strings = vec!["xyzw", "zone", "bbba", "abcd", "bbbb", "gfre", "acdf", "qwer", "bgfd", "zsdw"];
+    radix_sort(&mut strings);
 }
